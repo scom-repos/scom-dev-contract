dc9b31394a328025e0a48a8367c5a7bb6a68410d
benho-ijs <benho@instajs.com>
2024-06-11 18:17:59 +0800

    hardhat

--- hardhat/hardhat-mocks.config.ts
+++ hardhat/hardhat-mocks.config.ts
@@ -1,6 +1,4 @@
 import { HardhatUserConfig } from "hardhat/config";
-import "solidity-coverage";
-import "@nomiclabs/hardhat-web3";
 
 const config: HardhatUserConfig = {
   solidity: {
@@ -13,18 +11,23 @@ const config: HardhatUserConfig = {
             runs: 999999
           }
         }
+      },
+      {
+        version: "0.4.26",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          }
+        }
       }
     ]
   },
   paths: {
-    root: "../",
+    root: "../packages/mock-contracts/",
     sources: "./contracts", 
-    tests: "./test",
-    cache: "./hardhat/cache",
-    artifacts: "./hardhat/artifacts"
-  },
-  mocha: {
-    timeout: 1000000
+    cache: "../../hardhat/cache",
+    artifacts: "../../hardhat/artifacts"
   }
 };
 
